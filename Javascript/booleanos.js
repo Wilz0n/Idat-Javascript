8d1dd2e4f8e1f09d8be1af9d6c2b097fc00ca379
Wilson <Fishas02@gmail.com>
2024-05-15 21:02:45 -0500

    tarea2-Javascript

---
+++ Javascript/booleanos.js
@@ -0,0 +1,16 @@
+// Booleanos
+
+let temperatura = 25;
+let llueve = false;
+
+if (temperatura > 20) {
+    console.log("Hace calor.");
+} else {
+    console.log("Hace frío.");
+}
+
+if (llueve) {
+    console.log("Está lloviendo. Lleva un paraguas contigo.");
+} else {
+    console.log("No está lloviendo. Puedes salir sin paraguas.");
+}
---
+++ Javascript/string.js
@@ -0,0 +1,18 @@
+let texto = "Hola Mundo";
+
+// length 
+
+console.log(texto.length); // lo imprime 10
+
+// toUpperCase
+
+console.log(texto.toLocaleUpperCase()); // vuelve el texto en mayuscula el string en este caso "HOLA MUNDO"
+
+// toLowerCase
+
+console.log(texto.toLocaleLowerCase()); // vuelve el texto en minuscula el string en este caso "hola mundo"
+
+// trim().length
+
+let parrafo = "       Hola Mundo     " // trim() este se utiliza para eliminar los espacios blancos al principio y al final del mismo 
+console.log(parrafo.trim().length);
