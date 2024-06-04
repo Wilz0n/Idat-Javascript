069a82216d173fd10a4e2adb31d6f4c0586ec35e
Wilson <Fishas02@gmail.com>
2024-06-03 22:30:41 -0500

    Tarea 11 JS Tienda de DOM inicio

---
+++ 03_PCarroCompras/js/app.js
@@ -0,0 +1,75 @@
+// PROYECTO
+// VARIABLES {1}
+ 
+const shoppingBasket = document.getElementById('shoppingBasket');
+const shoppingBasketList = document.querySelector('#shoppingBasketList tbody');
+const clearShoppingBasket = document.getElementById('clearShoppingBasket');
+const courseList = document.getElementById('courseList');
+let coursesShoppingBasket = [];
+ 
+loadListeners();
+ 
+// LISTENERS
+function loadListeners() {
+  courseList.addEventListener('click', addCourseShoppingBasket);
+}
+ 
+// FUNCTIONS
+ 
+function addCourseShoppingBasket() {
+  console.log('hola');
+}
+
+/// {2}
+
+function addCourseShoppingBasket(e) {
+    if (e.target.classList.contains('addShoppingBasket')) {
+      const courseSelected = e.target.parentElement.parentElement;
+   
+      readCourseToShoppingBasket(courseSelected);
+    }
+  }
+   
+  function readCourseToShoppingBasket(courseSelected) {
+    const dataCourseSelected = {
+      id: courseSelected.querySelector('a').getAttribute('data-id'),
+      image: courseSelected.querySelector('img').src,
+      title: courseSelected.querySelector('h4').textContent,
+      price: courseSelected.querySelector('.precio span').textContent,
+      quantity: 1,
+    };
+   
+    coursesShoppingBasket = [...coursesShoppingBasket, dataCourseSelected];
+   
+    printCourseShoppingBasket();
+  }
+   
+  function printCourseShoppingBasket() {
+    console.log('Pintaremos el array');
+  }
+
+  /// {3}
+
+  function printCourseShoppingBasket() {
+    shoppingBasketList.textContent = '';
+   
+    coursesShoppingBasket.forEach((course) => {
+      const { id, image, title, price, quantity } = course;
+   
+      const row = document.createElement('tr');
+   
+      row.innerHTML = `
+        <td><img src="${image}" /></td>
+        <td>${title}</td>
+        <td>${quantity}</td>
+        <td>${price}</td>
+        <td> <a  class="deleteCourse" data-id="${id}" href="#">X</a></td>
+      `;
+   
+      shoppingBasketList.appendChild(row);
+    });
+   
+    // console.log(row);
+   
+    // console.log('Pintaremos el array');
+  }
